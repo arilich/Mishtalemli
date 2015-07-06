9f623430413224ab5130366213ebd084b0c83f30
ArielLichter <ariellichter@gmail.com>
2015-07-06 12:36:03 +0300

    Update
    first update

--- Mishtalemli/app.js
+++ Mishtalemli/app.js
@@ -1,3 +1,38 @@
-/**
- * Created by Ariel on 06/07/2015.
- */
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ port: 80 });
+
+server.views({
+    engines: {
+        html: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: './views'
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply.view('index.html');
+    }
+});
+
+server.route({
+    method: 'POST',
+    path: '/',
+    handler: function (request, reply) {
+        if (request.payload.email) {
+            var data = {email : request.payload.email, password : request.payload.password};
+            console.log(data);
+            reply.view('search.html', data);
+        } else {
+            console.log('no email');
+        }
+    }
+});
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
