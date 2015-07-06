52396e5def70829983655088eb5b482bef81faa5
ArielLichter <ariellichter@gmail.com>
2015-07-06 15:40:42 +0300

    ebay api update

--- Mishtalemli/app.js
+++ Mishtalemli/app.js
@@ -54,7 +54,7 @@ server.route({
     path: '/search',
     handler: function (request, reply) {
         if(request.payload.search){
-
+            ebaysearch(request.payload.search);
         }else{
             reply.view('search.html', {email: request.payload.email});
         }
@@ -65,3 +65,52 @@ server.route({
 server.start(function () {
     console.log('Server running at:', server.info.uri);
 });
+
+function ebaysearch(keywords) {
+    // example simple request to FindingService:findItemsByKeywords
+
+    var ebay = require('ebay-api');
+
+    var id = 'nonamec97-dbe0-4791-9985-731e31e5d36';
+
+    var params = {};
+
+    params.keywords = [keywords];
+
+// add additional fields
+//params.outputSelector = [ 'AspectHistogram' ];
+
+//params['paginationInput.entriesPerPage'] = 5;
+
+
+    var filters = {};
+
+//filters.itemFilter = [
+//    new ebay.ItemFilter("FreeShippingOnly", true)
+//];
+
+//filters.domainFilter = [
+//    new ebay.ItemFilter("domainName", "Digital_Cameras")
+//];
+
+
+    ebay.ebayApiGetRequest({
+            serviceName: 'FindingService',
+            opType: 'findItemsByKeywords',
+            appId: id,      // FILL IN YOUR OWN APP KEY, GET ONE HERE: https://publisher.ebaypartnernetwork.com/PublisherToolsAPI
+            params: params,
+            filters: filters,
+            parser: ebay.parseItemsFromResponse    // (default)
+        },
+        // gets all the items together in a merged array
+        function itemsCallback(error, items) {
+            if (error) throw error;
+
+            console.log('Found', items.length, 'items');
+            
+            for (var i = 0; i < items.length; i++) {
+                console.log('- ' + items[i].title);
+            }
+        }
+    );
+}
